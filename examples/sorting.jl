43d8f76a2f9ba313ab329f4b88714d56f1ede06e
McCoy R. Becker <mccoybecker@gmail.com>
2020-07-26 18:38:50 -0700

    Init.

---
+++ examples/sorting.jl
@@ -0,0 +1,73 @@
+module Sorting
+
+include("../src/Synthesizer.jl")
+using .Synthesizer
+
+# ------------ Example usage 2 ------------ #
+
+# A small array operation language.
+swap!(x::Array) = begin
+    if length(x) != 1
+        x[1], x[end] = x[end], x[1]
+    end
+    return x
+end
+
+swap_tail!(x::Array) = begin
+    if length(x) != 1
+        x[end - 1], x[end] = x[end], x[end - 1]
+    end
+    return x
+end
+
+swap_head!(x::Array) = begin
+    if length(x) != 1
+        x[1], x[2] = x[2], x[1]
+    end
+    return x
+end
+
+reorder(x::Array) = begin
+    new = typeof(x)()
+    if length(x) != 1
+        len = length(x)
+        if len % 2 == 0
+            fst = x[1 : Int(floor(len / 2))]
+            snd = x[Int(ceil(len / 2)) + 1 : end]
+            append!(snd, fst)
+        else
+            fst = x[1 : Int(floor(len / 2))]
+            snd = x[Int(ceil(len / 2)) : end]
+            append!(snd, fst)
+        end
+    end
+    return snd
+end
+
+@lang (array_operation) _1 expr = begin
+    0.20 => swap!(_1)
+    0.20 => reorder(_1)
+    0.20 => swap_head!(_1)
+    0.20 => swap_tail!(_1)
+    0.20 => reverse(_1)
+end
+
+# Some Boolean holes.
+@lang (boolean) expr = begin
+    0.7 => true
+    0.3 => false
+end
+
+function check(x::Array{T}) where T <: Number
+    length(x) == 1 && return true
+    i = x[1]
+    for j in x[2 : end]
+        if !(i < j)
+            return false
+        end
+        i = j
+    end
+    return true
+end
+
+end # module
--- src/Synthesizer.jl
+++ src/Synthesizer.jl
@@ -96,28 +96,10 @@ macro lang(name, args, expr)
     fn
 end
 
-# ------------ Example usage 1 ------------ #
-
-# Declare a probabilistic DSL.
-@lang (arithmetic) expr = begin
-    0.2 => expr * expr
-    0.3 => expr + expr
-    0.4 => 2.0
-    0.1 => 1.0
-end
-
-ret, cl = simulate(arithmetic)
-
 # Insert a hole into a function.
 hole(lang::Function) = rand(gensym(), lang)
 hole(addr::Jaynes.Address, lang::Function) = rand(addr, lang)
 hole(addr::Jaynes.Address, lang::Function, args...) = rand(addr, lang, args...)
-
-function foo(x::Float64)
-    y = x * hole(1, arithmetic)
-    q = hole(2, arithmetic)
-    return y * q
-end
 
 function synthesize(samples::Int, fn::Function, args::Tuple, d::Distribution; reject = -Inf)
     score = Float64[]
@@ -130,89 +112,6 @@ function synthesize(samples::Int, fn::Function, args::Tuple, d::Distribution; re
         end
     end
     return Jaynes.Particles(calls, score, 0.0)
-end
-
-# Score a bunch of samples from the grammar.
-#ps = synthesize(100, foo, (5.0, ), Normal(5.0, 1.0); reject = -Inf)
-#map(1:length(ps)) do i
-#    display(ps.calls[i].trace)
-#    println(ps.lws[i])
-#end
-
-# ------------ Example usage 2 ------------ #
-
-# A small array operation language.
-swap!(x::Array) = begin
-    if length(x) != 1
-        x[1], x[end] = x[end], x[1]
-    end
-    return x
-end
-
-swap_tail!(x::Array) = begin
-    if length(x) != 1
-        x[end - 1], x[end] = x[end], x[end - 1]
-    end
-    return x
-end
-
-swap_head!(x::Array) = begin
-    if length(x) != 1
-        x[1], x[2] = x[2], x[1]
-    end
-    return x
-end
-
-reorder(x::Array) = begin
-    new = typeof(x)()
-    if length(x) != 1
-        len = length(x)
-        if len % 2 == 0
-            fst = x[1 : Int(floor(len / 2))]
-            snd = x[Int(ceil(len / 2)) + 1 : end]
-            append!(snd, fst)
-        else
-            fst = x[1 : Int(floor(len / 2))]
-            snd = x[Int(ceil(len / 2)) : end]
-            append!(snd, fst)
-        end
-    end
-    return snd
-end
-
-@lang (array_operation) _1 expr = begin
-    0.20 => swap!(_1)
-    0.20 => reorder(_1)
-    0.20 => swap_head!(_1)
-    0.20 => swap_tail!(_1)
-    0.20 => reverse(_1)
-end
-
-# Some Boolean holes.
-@lang (boolean) expr = begin
-    0.7 => true
-    0.3 => false
-end
-
-function check(x::Array{T}) where T <: Number
-    length(x) == 1 && return true
-    i = x[1]
-    for j in x[2 : end]
-        if !(i < j)
-            return false
-        end
-        i = j
-    end
-    return true
-end
-
-function foo(x::Array{T}) where T <: Number
-    i = 0
-    while !check(x)
-        x = hole(:hole => i, array_operation, x)
-        i += 1
-    end
-    return x
 end
 
 function synthesize(fn::Function, pairs::Array{T}; iters = 1000) where T <: Tuple
@@ -228,15 +127,6 @@ function synthesize(fn::Function, pairs::Array{T}; iters = 1000) where T <: Tupl
         iters -= 1
     end
     return nothing, nothing
-end
-
-ret, cl = synthesize(foo, [(reverse([i for i in 1 : 100]), [i for i in 1 : 100])])
-
-# Display.
-ret != nothing && cl != nothing && begin
-    display(cl.trace)
-    println(cl.score)
-    println(ret)
 end
 
 end # module
